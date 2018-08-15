b7b58c751036a0637c027f588445d09eb06222b4
Mindlessmrawesome <42394325+Mindlessmrawesome@users.noreply.github.com>
2018-08-14 18:09:41 -0700

    INIT
    this is somehting whitepau tolkd me to do

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+
+namespace AmmoMod
+{
+
+
+    class QPatch
+    {
+        public static void Main()
+        {
+            var harmony = HarmonyInstance.Create("mindless.ttmm.ammo.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    public class Class1
+    {
+
+    }
+}
